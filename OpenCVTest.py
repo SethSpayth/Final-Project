9dc6c57767084a44dbb079e9c91388d52f79f1ab
AlexPHebert2000 <119898626+AlexPHebert2000@users.noreply.github.com>
2022-12-13 13:22:27 -0600

    Update OpenCVTest.py
    This is a new version of the test I have been working on. This uses the dictionary method to detect the letter "A"

--- OpenCVTest.py
+++ OpenCVTest.py
@@ -2,105 +2,162 @@
 from cvzone.HandTrackingModule import HandDetector
 import cv2
 from time import monotonic
-import matplotlib.pyplot as plot
+import numpy as np
 
-#Define Constants
-##delay between each input can be accepted 
-INPUT_DELAY = 5
+#Global Variables
+#Debug Variable
+DEBUG = False
+##Timer Related Variables
+delayTimerEndTime = 0
 
-##Defalut value for distance -- ONLY USED BECAUSE CODE IS WRITTEN POORLY
-DEFAULT_VAL = 34_000
+#List of point pairs to measure
+measurementPairs = [
+    
+    #Connect Neighbors
+    (4, 8), #Thumb tip to index tip
+    (8, 12), #Index tip to middle tip
+    (12, 16), #Middle tip to ring tip
+    (16, 20), #Ring tip to pinky tip
+   
+    #Connect tips to bottom of hand
+    (4, 0), #Thumb to bottom of hand
+    (8, 0), #Index to bottom of hand
+    (12, 0), #Middle to bottom of hand
+    (16, 0), #Ring to bottom of hand
+    (20, 0), #Pinky to bottom of hand
+   
+    #Connect thumb tip to other tips
+    (4, 12), #Thumb to middle
+    (4, 16), #Thumb to ring
+    (4, 20) #Thumb to pinky
 
-#Initialize the video capture and hand detector
+]
+
+#Dictionary of letters represented by their corresponding 
+#measurement value list
+alphaDict = {
+    
+    "A" : [75,25,30,30,180,100,85,80,80,100,115,130,45]
+    
+}
+
+#function that finds the angle of the hand
+def findAngle(hand, img = None):
+    
+    #base and center landmarks
+    baseX, baseY, z = hand["lmList"][0]
+    middleFingerX, middleFingerY, z = hand["lmList"][9]
+    
+    #define vertical line
+    vert = (baseX, 0)
+    
+    #create vectors
+    baseToPalm = np.array([middleFingerX, middleFingerY]) - np.array([baseX, baseY])
+    baseToTop = np.array(vert) - np.array([baseX, baseY])
+    
+    #calculate angle
+    angle = np.degrees(np.math.atan2(np.linalg.det([baseToPalm, baseToTop]), np.dot(baseToPalm, baseToTop)))
+    
+    #if image is passed, print the vectors and return value
+    if img is not None:
+        cv2.line(img, (baseX,baseY), (middleFingerX, middleFingerY), (225,225,0), 3)
+        cv2.line(img, (baseX,baseY), (vert[0],vert[1]), (225,225,0), 3)
+        return angle, img
+    
+    #otherwise just return value
+    else:
+        return angle                
+
+#Function that checks the input timer
+def inputDelayTimer():
+    if monotonic() > delayTimerEndTime:
+        return True
+    else:
+        return False
+
+#Function that sets the input delay timer
+def startInputDelayTimer(delayTime):
+    global delayTimerEndTime
+    delayTimerEndTime = monotonic() + delayTime
+
+#Function to get mesurements (defined by measurementPairs)
+#and angle
+def getMeasurements(hand, img = None):
+    #reset measurement array
+    measurements = []
+                
+    #measure distances
+    for x, y in measurementPairs:
+        measurement, info = detector.findDistance(rightLandmarks[x], rightLandmarks[y])
+        measurements.append(measurement)
+    
+    #if debug is on, print values and display angle lines
+    if DEBUG and img is not None:
+        angle, img = findAngle(rightHand, img)
+        measurements.append(angle)      
+        #print all measurements
+        print (f"measurements:{measurements}\nangle{angle}")
+        return measurements, img
+    
+    else:
+        #Calculate angle of hand and add to measurement list
+        angle = findAngle(rightHand)
+        measurements.append(angle)
+        return measurements
+   
+#initialize the video capture and hand detector
 cap = cv2.VideoCapture(0)
-detector = HandDetector(detectionCon=0.8,maxHands=1)
-
-#Iititalize varibles
-pauseEnd = 0
-pauseStart = 0
-fingers1 = []
-fingers2 = []
-thumbToJoint2Index = DEFAULT_VAL
-
-xValues = []
-yValues = []
-fingerTips = [4,8,12,16,20]
-palmBase = 0
-firstKnuckle = [2,5,9,13,17]
+detector = HandDetector(detectionCon=0.8, maxHands=1)
 
 #Operational Loop
 while True:
     
-    #Capture Video and find hands
+    #Capture video and find hands
     success, img = cap.read()
-    hands,img = detector.findHands(img, draw=True)
+    hands, img = detector.findHands(img, draw=True)
     
-    #If it detects a hand
+    #If hand is detected
     if hands:
-        # Hand 1
-        hand1 = hands[0]
         
-        #create matrix of points on the hand
-        lmList1 = hand1["lmList"]
-        
-        #detect how many fingers are up 
-        #translated into a list from thumb to pinky; 1 is up and 0 is down
-        fingers1 = detector.fingersUp(hand1)
-        
-        hand1Center = hand1["center"]
-        
-        #unpack position of index fingertip
-        x,y,z = lmList1[fingerTips[1]]
-        #take the point and add them to the coodinate lists
-        xValues.append(x)
-        yValues.append(y)
-        
-        #measure distance between thumb tip and middle tip
-        length, info = detector.findDistance(lmList1[fingerTips[0]], lmList1[fingerTips[2]])
-        #if they are touching, leave operational loop
-        if length < 20:
-            break
-        
-        #measure thumb tip to bottom joint of the index finger
-        thumbToJoint2Index, info = detector.findDistance(lmList1[4], lmList1[6])
-        
-        #measure the thumb tip to center of palm
-        thumbToPalm, info = detector.findDistance(lmList1[4], hand1Center)
-        
-        #If thumb is up and all other fingers are down and thumb is touching bottom index joint
-        if fingers1 == [1,0,0,0,0] and thumbToJoint2Index < 20 and input is False:
+        #Check if it is the right hand
+        if hands[0]["type"] == "Right":
             
-            #print "A"
-             print ("A")
-             
-             #Start input delay timer
-             input = True
-             pauseEnd = monotonic() + INPUT_DELAY
+            #Define hand and landmarks
+            rightHand = hands[0]
+            rightLandmarks = rightHand["lmList"]
+            rightCenter = rightHand["center"]
+            
+            #check input delay timer
+            if inputDelayTimer():
+                
+                #measure hand, print if debug
+                if not DEBUG:
+                    measurements = getMeasurements(rightHand)
+                else:
+                    measurements, img = getMeasurements(rightHand, img)
+                
+                try:
+                    #for each letter in the letter list
+                    for letter in alphaDict:
+                        #for each value in measurements
+                        for pos, val in enumerate(measurements):
+                            #check if the value is less than the value 
+                            #in the matching position in the letter
+                            if val > float(alphaDict[letter][pos]):
+                                break
+                        
+                            #if the last check passes print the letter 
+                            #and delay input for 10 seconds
+                            elif pos == len(measurements) - 1:
+                                print(letter)
+                                startInputDelayTimer(10)
+                                
+                                #break loop
+                                raise StopIteration
+                            
+                except StopIteration:
+                    pass
 
-        #If thumb is down and all other fingers are up and thumb is touching center of palm
-        elif fingers1 == [0,1,1,1,1] and thumbToPalm < 20:
-            
-            #print B
-            print ("B")
-            
-            #Start input delay timer
-            input = True
-            pauseEnd = monotonic() + INPUT_DELAY
-            
-        #If input timer is running and full delay has elapsed
-        #End delay timer and reset input
-        elif monotonic() >= pauseEnd:
-            input = False
-            print("input reset")
-    
-    #display image and cleanup delay
+    #Show image
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-    
-#Cleanup
-cap.release()
-cv2.destroyAllWindows()
-
-#plot and display the values the index finger tip took
-plot.plot(xValues, yValues)
-plot.show()
